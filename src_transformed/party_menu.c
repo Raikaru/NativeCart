634a2a94c7d03b3b2f99fee73b3227a86f7c9c29
Raikaru <raikarualt001@gmail.com>
2026-03-20 10:59:59 -0400

    Fix portable text encoding and UI string rendering

--- src_transformed/party_menu.c
+++ src_transformed/party_menu.c
@@ -2320,7 +2320,7 @@ static void DisplayPartyPokemonLevelCheck(struct Pokemon *mon, struct PartyMenuB
 static void DisplayPartyPokemonLevel(u8 level, struct PartyMenuBox *menuBox)
 {
     ConvertIntToDecimalStringN(gStringVar2, level, STR_CONV_MODE_LEFT_ALIGN, 3);
-    StringCopy(gStringVar1, gText_Lv);
+    StringExpandPlaceholders(gStringVar1, gText_Lv);
     StringAppend(gStringVar1, gStringVar2);
     DisplayPartyPokemonBarDetail(menuBox->windowId, gStringVar1, 0, &menuBox->infoRects->dimensions[4]);
 }
